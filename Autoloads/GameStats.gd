b6a17f0781fe22535fd50359362dd8914739f99c
mjcutshaw <mjcutshaw@gmail.com>
2022-06-19 23:46:06 -0400

    timer formatting

--- Autoloads/GameStats.gd
+++ Autoloads/GameStats.gd
@@ -9,8 +9,9 @@ func _ready():
 
 func _process(delta):
 	time += delta
-	var ms = fmod(time,1)*1000
-	var seconds = fmod(time,60)
-	var minutes = fmod(time, 3600) / 60
-	strTimeElapsed = "%02d : %02d . %02d" % [minutes, seconds, ms]
+	var mils = fmod(time,1)*1000
+	var secs = fmod(time,60)
+	var mins = fmod(time, 3600) / 60
+	var hr = fmod(time,3600 * 60) / 3600
+	strTimeElapsed = "%02d : %02d : %02d . %03d" % [hr, mins, secs, mils]
 #	print("elapsed : ", strTimeElapsed)
