27d461f491336af5a116b010a423afb9dffe0fd6
mjcutshaw <mjcutshaw@gmail.com>
2022-06-25 23:38:10 -0400

    boost block

--- Prefab/AbilityBlocks/Scripts/Ability_Boost.gd
+++ Prefab/AbilityBlocks/Scripts/Ability_Boost.gd
@@ -1,14 +1,30 @@
 extends Area2D
 
-#TODO: boost the player in a specific direction, can be ability based
+#FIXME: kinda works
 var Abilities: Resource = preload ("res://Actor/Player/Resources/PlayerAbilities.tres")
+@onready var raycastBoost: RayCast2D = $RayCast2D
 @export var ability: PlayerAbilities.abiliyList
+@export var rotateBlock: bool = false
+var boostDirection: Vector2
+var boostAmount: int = 200
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	body_entered.connect(boost)
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	
+	
+	if rotateBlock:
+		rotate(.1)
+
+func boost(body) -> void:
+	var rGlobalOrigin = raycastBoost.to_global(Vector2.ZERO)
+	var rGlobalCastToEndpoint = raycastBoost.to_global(raycastBoost.target_position)
+	boostDirection = rGlobalCastToEndpoint - rGlobalOrigin
+	
+	body.velocity = boostDirection * boostAmount
+	print(boostDirection)
+	
