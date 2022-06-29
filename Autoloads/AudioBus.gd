c5082bdaa92c6f935dc798f7428105f0bb98875f
mjcutshaw <mjcutshaw@gmail.com>
2022-06-28 23:13:22 -0400

    created audio manager

--- Autoloads/AudioBus.gd
+++ Autoloads/AudioBus.gd
@@ -1,3 +1,41 @@
 extends Node2D
-#FIXME: make this
+#TODO: set up paused signals
 
+enum audioType {music, background, sfx}
+
+@onready var musicBus: Node2D = $Music
+@onready var backgroundBus: Node2D = $Backgrounds
+@onready var sfxBus: Node2D = $SFX
+
+
+func play_sound(type: int, clipName: AudioStream, volume: float = 0.0, pitch: float = 1.0) -> void:
+	if type == audioType.music:
+		print("need to create music")
+	elif type == audioType.background:
+		print("need to create background")
+	elif type == audioType.sfx:
+		_play_sfx(clipName, volume, pitch)
+	else:
+		print("error player: " + clipName._get_stream_name() + ", unknown type")
+
+
+
+#func _play_music(clipName, loop: bool) -> void:
+#	pass
+#
+#
+#func _stop_music(clipName) -> void:
+#	pass
+#
+#
+#func _pause_music(clipName) -> void:
+#	pass
+
+
+func _play_sfx(clipName, volume: float = 0.0, pitch: float = 1.0) -> void:
+	for audioPlayer in sfxBus.get_children():
+		if audioPlayer.playing == false:
+			audioPlayer.stream = clipName
+			audioPlayer.volume_db = volume
+			audioPlayer.pitch_scale = pitch
+			audioPlayer.play()
--- Prefab/Interactables/Scripts/Collectable.gd
+++ Prefab/Interactables/Scripts/Collectable.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 #TODO: send sound to audio bus
 var Abilities = ResourceLoader.load("res://Actor/Player/Resources/PlayerAbilities.tres")
+var sound = preload("res://Assests/Sounds/SUCCESS PICKUP Collect Chime 01.ogg")
 @export var ability: PlayerAbilities.abiliyList
 
 
@@ -9,6 +10,7 @@ func _on_collectable_body_entered(_body) -> void:
 	#TODO: turn into signal
 	Abilities.unlock_ability(ability)
 	EventBus.emit_signal("ability_unlocked", ability)
-	$SoundUnlocked.play()
+#	$SoundUnlocked.play()
+	AudioBus.play_sound(AudioBus.audioType.sfx, sound)
 	EventBus.emit_signal("ability_check")
 	queue_free()
