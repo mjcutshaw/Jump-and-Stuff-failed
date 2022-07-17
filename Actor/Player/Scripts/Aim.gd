6442cfd655c177a2a3ec0df20e423a439623fa59
mjcutshaw <mjcutshaw@gmail.com>
2022-07-17 13:44:08 -0400

    aim working

--- Actor/Player/Scripts/Aim.gd
+++ Actor/Player/Scripts/Aim.gd
@@ -10,24 +10,31 @@ func _ready() -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if player.aimDirection != Vector2.ZERO:
-		aimIndicator.visible = true
-		aim()
-	else:
-		aimIndicator.visible = false
+	pass
 
 func _physics_process(delta: float) -> void:
 	aim_direction()
+	
+	
+	
+	if player.aimDirection == Vector2.ZERO:
+		aimIndicator.visible = false
+	else:
+		aimIndicator.visible = true
+		aim()
 
 
 func aim_direction() -> void:
-	player.aimDirection =  Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
-	
+	var input_strength: Vector2 =  Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+	var deadzone_radius: = 0.2
+	if input_strength.length() > deadzone_radius:
+		player.aimDirection = input_strength
+	else:
+		player.aimDirection = Vector2.ZERO
 
 func aim()-> void:
-	var cast: Vector2 = player.aimDirection * raycast.target_position.length()
+	var cast: Vector2 = player.aimDirection.normalized() * raycast.target_position.length()
 	var angle: = cast.angle()
-#	rotation = angle
 	raycast.target_position = cast
 	aimIndicator.rotation = angle
 	detector.rotation = angle
